7ff6d1c24fef95aea8067a6576b744a6e142cecb
meroshanpandey <pandeyroshan556@gmail.com>
2020-07-06 18:19:54 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Captch Bypass - JUET Webkiosk
+Bypass the captch of JUET Webkiosk using just 3 lines of effective code.
---
+++ bypass.js
@@ -0,0 +1,4 @@
+var a = document.getElementsByClassName("noselect")
+
+var x = document.getElementsByName("txtcap")
+x[0].value = a[0].innerHTML
