fa6bc0e984fa96116b581c39e4f0f8d1f5d74f95
Roshan Pandey <pandeyroshan556@gmail.com>
2020-09-03 15:26:52 +0530

    Update bypass.js

--- bypass.js
+++ bypass.js
@@ -1,4 +1,14 @@
 var a = document.getElementsByClassName("noselect")
+console.log(a[0].innerHTML)
 
 var x = document.getElementsByName("txtcap")
 x[0].value = a[0].innerHTML
+
+var y = document.getElementsByName("MemberCode")
+y[0].value = 'your_enrollment_here'
+
+var z = document.getElementsByName("DATE1")
+z[0].value = 'your_db_here'
+
+var w = document.getElementsByName("Password")
+w[0].value = 'your_password_here'
